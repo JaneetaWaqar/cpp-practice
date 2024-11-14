1757b34030b74cfbe0527e50d806c7296264fd11
HammadMaqbool <hammad.mqbl@gmail.com>
2024-11-14 13:48:14 +0500

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int abc[3];
+	abc[0] = 10;
+	abc[1] = 15;
+	abc[2] = 25;
+	abc[3] = 55;
+	abc[4] = 86;
+	abc[5] = 65;
+	
+	cout<<abc[4];
+}
